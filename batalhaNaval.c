1d007fc8455afc688800702a624de84f025e33d3
Antônio Azevedo Saraiva <157730092+Antonio-Azevedo-Saraiva@users.noreply.github.com>
2025-10-07 13:52:10 +0000

    codigo corrigido no formato de cone, cruz e octaedro# Changes not staged for commit:

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,3 +1,18 @@
+// Função para sobrepor e marcar cada habilidade individualmente
+void sobrepor_marca(int efeito[10][10], int habilidade[5][5], int origem_l, int origem_c) {
+    int offset = 2;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int l = origem_l + (i - offset);
+            int c = origem_c + (j - offset);
+            if (l >= 0 && l < 10 && c >= 0 && c < 10) {
+                if (habilidade[i][j] == 1) {
+                    efeito[l][c] = 1;
+                }
+            }
+        }
+    }
+}
 
 #include <stdio.h>
 
@@ -19,13 +34,15 @@ void sobrepor_habilidade(int tab[10][10], int habilidade[5][5], int origem_l, in
 
 int main() {
     // Matrizes de habilidades (5x5)
+    // Cone: triângulo para baixo
     int cone[5][5] = {
-        {1, 0, 0, 0, 0},
-        {1, 1, 0, 0, 0},
-        {1, 1, 1, 0, 0},
-        {1, 1, 1, 1, 0},
-        {1, 1, 1, 1, 1}
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
     };
+    // Cruz: linhas e colunas centrais
     int cruz[5][5] = {
         {0, 0, 1, 0, 0},
         {0, 0, 1, 0, 0},
@@ -33,11 +50,12 @@ int main() {
         {0, 0, 1, 0, 0},
         {0, 0, 1, 0, 0}
     };
+    // Octaedro: losango centralizado
     int octaedro[5][5] = {
         {0, 0, 1, 0, 0},
-        {0, 1, 1, 1, 0},
-        {1, 1, 1, 1, 1},
-        {0, 1, 1, 1, 0},
+        {0, 1, 0, 1, 0},
+        {1, 0, 0, 0, 1},
+        {0, 1, 0, 1, 0},
         {0, 0, 1, 0, 0}
     };
 
@@ -100,12 +118,17 @@ int main() {
     int origem_cruz_l = 5, origem_cruz_c = 5;
     int origem_octaedro_l = 7, origem_octaedro_c = 7;
 
-    // Sobrepor habilidades ao tabuleiro
-    sobrepor_habilidade(tabuleiro_habilidade, cone, origem_cone_l, origem_cone_c);
-    sobrepor_habilidade(tabuleiro_habilidade, cruz, origem_cruz_l, origem_cruz_c);
-    sobrepor_habilidade(tabuleiro_habilidade, octaedro, origem_octaedro_l, origem_octaedro_c);
+    // Matrizes auxiliares para marcar cada habilidade separadamente
+    int efeito_cone[10][10] = {0};
+    int efeito_cruz[10][10] = {0};
+    int efeito_octaedro[10][10] = {0};
+
+
+    sobrepor_marca(efeito_cone, cone, origem_cone_l, origem_cone_c);
+    sobrepor_marca(efeito_cruz, cruz, origem_cruz_l, origem_cruz_c);
+    sobrepor_marca(efeito_octaedro, octaedro, origem_octaedro_l, origem_octaedro_c);
     // Exibe o tabuleiro com áreas de efeito
-    printf("Tabuleiro Batalha Naval (0 = água, 3 = navio, 5 = habilidade):\n\n");
+    printf("Tabuleiro Batalha Naval:\nC = Cone, X = Cruz, O = Octaedro, N = Navio, ~ = Água\n\n");
     // Cabeçalho das colunas
     printf("   ");
     for (int j = 0; j < 10; j++) {
@@ -116,10 +139,14 @@ int main() {
     for (int i = 0; i < 10; i++) {
         printf("%2d|", i);
         for (int j = 0; j < 10; j++) {
-            if (tabuleiro_habilidade[i][j] == 3) {
+            if (tabuleiro[i][j] == 3) {
                 printf(" N "); // Navio
-            } else if (tabuleiro_habilidade[i][j] == 5) {
-                printf(" * "); // Área de habilidade
+            } else if (efeito_cone[i][j]) {
+                printf(" C "); // Cone
+            } else if (efeito_cruz[i][j]) {
+                printf(" X "); // Cruz
+            } else if (efeito_octaedro[i][j]) {
+                printf(" O "); // Octaedro
             } else {
                 printf(" ~ "); // Água
             }
